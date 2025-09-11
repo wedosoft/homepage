1670c9de51285b50c67fef5f5f2f63b421dcf7ea
wedosoft-alan <wedosoft-alan@users.noreply.github.com>
2025-09-11 08:57:05 +0000

    deploy: 3d6800f4fe1e854338bd53bd19a7163a5f13f490

---
+++ _next/static/TyRqmtVwtFZvjHtEfJRsD/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/83d5c85ceea0244d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/83d5c85ceea0244d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/975ea5cf93d6229f.js",
+  "static/chunks/turbopack-f2844c986b12bda3.js"
+])
